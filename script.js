a837bf0a87f4d46fa60a57731dd73baa20c097af
tonyhento <131386469+tonyhento@users.noreply.github.com>
2026-03-10 13:20:41 -0300

    add projetos
    adicionei um botao para trocar o tema de fundo do site (claro e escuro) e comecei a deixar arrumado onde vai mostrar os projetos que realizei

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function atualizarHora(){
 
 setInterval(atualizarHora,1000);
 atualizarHora();
+
+/* TOGGLE TEMA */
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("light-mode")){
+        themeToggle.textContent = "🌙";
+    } else {
+        themeToggle.textContent = "☀";
+    }
+});
